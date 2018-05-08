cb81bdaf41dd182f689570ee53c57d1e3b248dac
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 10:45:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ def printer(attendees)
   attendees.each do |name|
     puts badge_maker(name)
   end
-  assign_rooms(attendees)
+  assigned rooms = assign_rooms(attendees)
   assigned_rooms.each do |room_info|
     puts room_info
   end
